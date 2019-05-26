bbfd61a92465d320fe0eaaf445ad9bde44b9b16d
Haroun BAKHOUCHE <darkopsis@gmail.com>
2019-05-26 18:22:41 +0200

    Amélioration Design + Programation JS en POO

--- Projet 3/js/carte-velov.js
+++ Projet 3/js/carte-velov.js
@@ -1,95 +1,96 @@
 {/////////////////////////////////////////////////// PARAMETRES DE L'API LEAFLET ///////////////////////////////////////////////////
 
 // https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=74d859ca150a0d64898b62eefd0255bc6f7704fa
-var mymap = L.map("mapid").setView([45.76, 4.86], 12.5); // variable les réglages de la map
 
-L.tileLayer(
-  // ajout du fond
-  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmFrb3VuIiwiYSI6ImNqdmNibnpqNDAxbDkzeW8zZnhtcmh0bGoifQ.Vj39v0WupOBNhy5r22UXyA",
-  {
-    maxZoom: 17,
-    id: "mapbox.streets"
-  }
-).addTo(mymap);
+class customMap {
+  constructor(mapId) {
+    this.mymap = L.map(mapId).setView([45.76, 4.86], 12.5);
+    L.tileLayer(
+      // ajout du fond
+      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmFrb3VuIiwiYSI6ImNqdmNibnpqNDAxbDkzeW8zZnhtcmh0bGoifQ.Vj39v0WupOBNhy5r22UXyA",
+      {
+        maxZoom: 16.5,
+        id: "mapbox.streets"
+      }
+    ).addTo(this.mymap);
 
-var velovIcon = L.Icon.extend({
-  options: {
-    shadowUrl: "../images/ombre.png",
-    iconSize: [30, 30],
-    shadowSize: [30, 30],
-    iconAnchor: [15, 28],
-    shadowAnchor: [13, 26],
-    popupAnchor: [0, -35]
-  }
-});
+    this.velovIcon = L.Icon.extend({
+      options: {
+        shadowUrl: "../images/ombre.png",
+        iconSize: [30, 30],
+        shadowSize: [30, 30],
+        iconAnchor: [15, 28],
+        shadowAnchor: [13, 26],
+        popupAnchor: [0, -35]
+      }
+    });
+    
+    this.redIcon = new this.velovIcon({iconUrl: "../images/velovRouge.png"});
 
-var greenIcon = new velovIcon({
-    iconUrl: "../images/velovVert.png"
-  }),
-  redIcon = new velovIcon({
-    iconUrl: "../images/velovRouge.png"
-  }),
-  orangeIcon = new velovIcon({
-    iconUrl: "../images/velovOrange.png"
-  });
-}
-{/////////////////////////////////////////////////// AFFICHAGE STATIONS VELO + FORMULAIRE DE RESERVATION ///////////////////////////////////////////////////
-let cluster = L.markerClusterGroup();
-var selectedStation;
+    this.cluster = L.markerClusterGroup();
+    this.selectedStation;
+    self = this;
+    this.zoom=this.mymap.getZoom();
+    console.log(this.zoom);
+    ajaxGet(
+      "https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=74d859ca150a0d64898b62eefd0255bc6f7704fa",
+      function(reponse) {
+        var listevelos = JSON.parse(reponse);
+        let formElt = document.querySelector("form"); //  Séléction des Elts à travers le DOM
+        let infosElt = document.getElementById("infos"); // *************
+        for (let velo of listevelos) {
+          let stationIcon = L.marker([velo.position.lat, velo.position.lng], {
+            icon: self.redIcon
+          }); // Création des marqueurs
+          self.cluster.addLayer(stationIcon); // ajout au plugin MarkerCluster
 
-ajaxGet(
-  "https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=74d859ca150a0d64898b62eefd0255bc6f7704fa",
-  function(reponse) {
-    var listevelos = JSON.parse(reponse);
-    let formElt = document.querySelector("form"); //  Séléction des Elts à travers le DOM
-    let infosElt = document.getElementById("infos"); // *************
-    for (let velo of listevelos) {
-      let stationIcon = L.marker([velo.position.lat, velo.position.lng], {
-        icon: redIcon
-      }); // Création des marqueurs
-      cluster.addLayer(stationIcon); // ajout au plugin MarkerCluster
+          stationIcon.addEventListener("click", function(e) {
+            // Fonction affichant info + formulaire réservation
+              self.mymap.setView([(velo.position.lat), (velo.position.lng+(0.021/(self.mymap.getZoom()-12)))], (self.mymap.getZoom()+1));
+              document.getElementById("mapid").style.flex = "0 0 65%";
+            self.selectedStation = velo;
+            document.getElementById("erreur").innerHTML = "";
+            formElt.classList.remove("hidden");
+            infosElt.innerHTML = `<span class ="brand-font">Station :</span> ${
+              velo.name.split("- ")[1]
+            } \r\n
+            <span class ="brand-font">Adresse</span> : ${velo.address} \r\n
+                    Il reste <b>${velo.available_bikes}</b> vélos sur les <b>${velo.bike_stands}</b> places disponibles`;
 
-      stationIcon.addEventListener("click", function(e) {
-        // Fonction affichant info + formulaire réservation
-        selectedStation = velo;
-        document.getElementById("erreur").innerHTML = "";
-        formElt.classList.remove("hidden");
-        infosElt.innerHTML = `<span class ="brand-font">Station :</span> ${
-          velo.name.split("- ")[1]
-        } \r\n
-        <span class ="brand-font">Adresse</span> : ${velo.address} \r\n
-                Il reste <b>${velo.available_bikes}</b> vélos sur les <b>${velo.bike_stands}</b> places disponibles`;
+            document.getElementById("signatureDiv").classList.add("hidden");
+            document.getElementById("erreur").classList.add("hidden");
+          });
 
-        document.getElementById("signatureDiv").classList.add("hidden");
-        document.getElementById("erreur").classList.add("hidden");
-      });
-
-      formElt.removeEventListener("submit", testStation);
-      var testStation = function(e) {
-        e.preventDefault();
-        if (
-          selectedStation.available_bikes > 0 &&
-          selectedStation.status === "OPEN"
-        ) {
-          document.getElementById("erreur").innerHTML = "";
-					document.getElementById("signatureDiv").classList.remove("hidden");
-					clearCanvas();
-        } else if (selectedStation.available_bikes === 0) {
-          document.getElementById("erreur").classList.remove("hidden");
-          document.getElementById("erreur").innerHTML =
-            "Station vide ! Vous ne pouvez pas réserver de vélo !";
-        } else if (selectedStation.status !== "OPEN") {
-          document.getElementById("erreur").classList.remove("hidden");
-          document.getElementById("erreur").innerHTML =
-            "Station indisponible ! Veuillez en choisir une autre !";
+          formElt.removeEventListener("submit", testStation);
+          var testStation = function(e) {
+            e.preventDefault();
+            if (
+              self.selectedStation.available_bikes > 0 &&
+              self.selectedStation.status === "OPEN"
+            ) {
+              document.getElementById("erreur").innerHTML = "";
+              document.getElementById("signatureDiv").classList.remove("hidden");
+              clearCanvas();
+            } else if (self.selectedStation.available_bikes === 0) {
+              document.getElementById("erreur").classList.remove("hidden");
+              document.getElementById("erreur").innerHTML =
+                "Station vide ! Vous ne pouvez pas réserver de vélo !";
+            } else if (self.selectedStation.status !== "OPEN") {
+              document.getElementById("erreur").classList.remove("hidden");
+              document.getElementById("erreur").innerHTML =
+                "Station indisponible ! Veuillez en choisir une autre !";
+            }
+          };
+          formElt.addEventListener("submit", testStation);
         }
-      };
-      formElt.addEventListener("submit", testStation);
-    }
-  }
-);
+      }
+    );
 
-mymap.addLayer(cluster); // Ajout des clusters à la map;
+    this.mymap.addLayer(this.cluster); // Ajout des clusters à la map;
+  }
+}
+
+var mapLyon = new customMap("mapid");
 }
 {/////////////////////////////////////////////////// GESTION DES RESERVATIONS ///////////////////////////////////////////////////
 
@@ -119,7 +120,6 @@ function reservation(){
 		let timerSecondes = 0;
 		let nomUser = document.getElementById("nom").value;
 		let prenomUser = document.getElementById("prenom").value;
-		console.log(timer);
 				if (signTest > 30) {
 					document.getElementById("erreur").classList.add("hidden");
 					document.getElementById("signatureDiv").classList.add("hidden");
@@ -144,7 +144,7 @@ function reservation(){
 	
 					document.getElementById("erreur").innerHTML = "";
 					document.getElementById("infosReservation").innerHTML = `<p class="mt-3 mx-auto">Votre vélo à bien été réservé au nom de <b> ${nomUser} ${prenomUser} </b> à l'adresse suivante : \r\n
-							<i>${selectedStation.address}.</i> Il restera disponible pendant encore <b><span id="minuteur">${timerMinutes}:${formattedTimerSecondes}</span></b> minutes.</p>`;
+							<i>${self.selectedStation.address}.</i> Il restera disponible pendant encore <b><span id="minuteur">${timerMinutes}:${formattedTimerSecondes}</span></b> minutes.</p>`;
 					
 					isReserved = true;
 				} else {
@@ -155,63 +155,75 @@ function reservation(){
 }
 }
 {/////////////////////////////////////////////////// DIAPORAMA ///////////////////////////////////////////////////
+class diapo {
+  constructor(containerElt, bouttonPlayElt){
+    this.target = containerElt;
+    var self = this;
+    this.slideIndex = 1;
+    this.play = true;
+    this.carousel = setInterval(function() {self.plusSlides(1);}, 5000);
+    this.playPauseBtn = bouttonPlayElt;
+    this.slides = this.target.getElementsByClassName("diapo");
 
-var slideIndex = 1;
-showSlides(slideIndex);
-let play = true;
-let carousel = setInterval(function() {plusSlides(1);}, 5000);
-let playPauseBtn = document.getElementById("playPause");
-
-
-playPauseBtn.addEventListener("click", function(e){
-  play = !play;
-  if(play === false){
-    clearInterval(carousel);
-    playPauseBtn.classList.remove("fa-play");
-    playPauseBtn.classList.add("fa-pause");
+    this.showSlides(this.slideIndex);
+    this.initEventListeners();
   }
-  else if (play === true)
-  {
-    playPauseBtn.classList.remove("fa-pause");
-    playPauseBtn.classList.add("fa-play");
-    carousel = setInterval(function() {plusSlides(1);}, 5000);
-  }
-});
 
-function plusSlides(n) {
-		showSlides((slideIndex += n));
+currentSlide(n) {
+    this.showSlides((this.slideIndex = n));
+  }
+
+plusSlides(n) {
+		this.showSlides((this.slideIndex += n));
 }
 
-document.addEventListener("keydown", function(e) {
-  if (e.keyCode === 37) {
-    //press Left
-    plusSlides(-1);
+showSlides(n) {
+    var i;
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+    for (i = 0; i < this.slides.length; i++) {
+      this.slides[i].style.display = "none";
+    }
+    this.slides[this.slideIndex - 1].style.display = "block";
   }
-  if (e.keyCode === 39) {
-    //press Right
-    plusSlides(1);
-  }
-});
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+initEventListeners() {
+      var self = this;
+      this.playPauseBtn.addEventListener("click", function(e){
+        self.play = !self.play;
+        if(self.play === false){
+          clearInterval(self.carousel);
+          self.playPauseBtn.classList.remove("fa-play");
+          self.playPauseBtn.classList.add("fa-pause");
+        }
+        else if (self.play === true)
+        {
+          self.playPauseBtn.classList.remove("fa-pause");
+          self.playPauseBtn.classList.add("fa-play");
+          self.carousel = setInterval(function() {self.plusSlides(1);}, 5000);
+        }
+      });
+
+    document.addEventListener("keydown", function(e) {
+      if (e.keyCode === 37) {
+        //press Left
+        self.plusSlides(-1);
+      }
+      if (e.keyCode === 39) {
+        //press Right
+        self.plusSlides(1);
+      }
+    });
+
+    document.getElementById('prev').addEventListener("click", function (e) {self.plusSlides(-1)});
+    document.getElementById('next').addEventListener("click", function (e) {self.plusSlides(1)});
+  }
+
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("diapo");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
-
-
-
+var diapoVelov = new diapo (document.getElementById("diapoContainer"),document.getElementById("playPause"));
 }
